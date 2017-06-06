c62d43fa4aefc1b8c1fd3edbe7513cd32f1874b6
Hannah Nordgren <hey@hereshannahs.info>
2017-06-06 12:30:46 -0400

    [FEATURE] mock story data fed to each story page

--- src/pages/Story/Story.js
+++ src/pages/Story/Story.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import StoryData from '../../data/stories_data.json';
 
-const Story = ({ allStories }) => {
-  console.log('storyData=', allStories)
+function storyCopy(storyNum){
+  const num = parseInt(storyNum, 10);
+  const currentStory = StoryData.videos.map(story => {
+    if (num === story.video) {
+      console.log('match!');
+      return story.copy
+    } else {
+      console.log('no match!')
+      return null;
+    }
+  })
+
+  const matchingStory = currentStory.filter(item => { return item !== null }).toString();
+
+  return {__html: matchingStory };
+}
+
+const Story = ({ match }) => {
+  const storyNum = match.params.number;
+
   return (
     <div className="story">
-      <h1>Individual Story Page Coming Soon!</h1>
+      <div dangerouslySetInnerHTML={storyCopy(storyNum)}/>
     </div>
   );
 }
