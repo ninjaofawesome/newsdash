f5d946e441ea1c84b575ab65998d97b9eed69feb
Hannah Nordgren <hey@hereshannahs.info>
2017-06-07 10:55:08 -0400

    [WIP] reconfiguring routes display for stories

--- src/pages/Stories/Stories.js
+++ src/pages/Stories/Stories.js
@@ -1,69 +1,14 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router';
-import { Link } from 'react-router-dom';
-import Card from '../../components/Card/Card';
-import StoryData from '../../data/stories_data.json';
-import Post from '../../components/Post/Post';
 import Story from '../../pages/Story/Story';
 import TopStories from '../../pages/TopStories/TopStories';
 
 
 class Stories extends Component {
   render(){
-    const allStories = StoryData.videos;
     return(
-      <div className="home-wrapper">
-        <div className="home__post-container">
-          <h2 className="home__post-list-title">Local</h2>
-          <ul className="home__post-list">
-            {allStories.map((item, index) =>{
-              return(
-                <Post
-                  key={`homePost-${index}`}
-                  className="post"
-                  title={item.title}
-                  author={item.author}
-                  video={item.video}
-                  section={item.section}
-                  listType="local"
-                />
-              );
-            })}
-          </ul>
-          <h2 className="home__post-list-title">National</h2>
-          <ul className="home__post-list">
-            {allStories.map((item, index) =>{
-              return(
-                <Post
-                  key={`homePost-${index}`}
-                  className="post"
-                  title={item.title}
-                  author={item.author}
-                  video={item.video}
-                  section={item.section}
-                  listType="national"
-                />
-              );
-            })}
-          </ul>
-          <h2 className="home__post-list-title">Entertainment</h2>
-          <ul className="home__post-list">
-            {allStories.map((item, index) =>{
-              return(
-                <Post
-                  key={`homePost-${index}`}
-                  className="post"
-                  title={item.title}
-                  author={item.author}
-                  video={item.video}
-                  section={item.section}
-                  listType="entertainment"
-                />
-              );
-            })}
-          </ul>
-        </div>
-        <div className="home__top-stories-container">
+      <div className="stories-wrapper">
+        <div className="stories__top-stories-container">
           <Switch>
             <Route exact path="/stories" component={TopStories} />
             <Route path="/stories/:number" component={Story} />
--- src/pages/TopStories/TopStories.js
+++ src/pages/TopStories/TopStories.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import StoryData from '../../data/stories_data.json';
+import Post from '../../components/Post/Post';
+import Card from '../../components/Card/Card';
 
 class TopStories extends Component {
   render(){
+    const allStories = StoryData.videos;
     return(
-      <h1>I am top Stories</h1>
+      <div className="top-stories">
+        <div className="top-stories__all-stories-container">
+          <div className="top-stories__all-stories-card-wrapper">
+            <Card
+              className="top-stories-card"
+              title={allStories[0].title}
+              image={allStories[0].image}
+              author={allStories[0].author}
+              video={allStories[0].video}
+              main={true}
+            />
+          </div>
+          <div className="top-stories__all-stories-wrapper">
+            <h2 className="top-stories__post-list-title">Breaking News</h2>
+            <ul className="top-stories__post-list">
+              {allStories.map((item, index) =>{
+                return(
+                  <Post
+                    key={`storiesPost-${index}`}
+                    className="post"
+                    title={item.title}
+                    author={item.author}
+                    video={item.video}
+                    section={item.section}
+                  />
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
